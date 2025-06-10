580070f2e528cb85a8522c7635986a2a4a641d68
Manujdixit <manujdixit@gmail.com>
2025-06-10 18:35:04 +0530

    college routes

--- backend/src/routes/college/college.ts
+++ backend/src/routes/college/college.ts
@@ -3,105 +3,25 @@ import { getTopColleges } from "../../controllers/college/topColleges";
 import { collegeInfo } from "../../controllers/college/info";
 import { getCollegeCoursesInfo } from "../../controllers/college/courses";
 import { getCollegeFeesInfo } from "../../controllers/college/fees";
+import { getCollegeDepartmentsInfo } from "../../controllers/college/departments";
+import { getCollegefaq } from "../../controllers/college/faqs";
+import { getCollegePlacementInfo } from "../../controllers/college/placement";
+import { getCollegeScholarshipInfo } from "../../controllers/college/scholarships";
+import { getCollegeRankingsInfo } from "../../controllers/college/ranking";
+import { getCollegeCareersInfo } from "../../controllers/college/careers";
 
 const CollegeRouter = express.Router();
 
-/**
- * @swagger
- * /api/v1/college/top:
- *   get:
- *     summary: Get top colleges
- *     tags: [Colleges]
- *     description: Retrieve a list of top-ranked colleges
- *     parameters:
- *       - in: query
- *         name: stream
- *         required: false
- *         description: Optional parameter to filter colleges by stream name (Engineering, Management, Design, Law)
- *         schema:
- *           type: string
- *           example: "engineering"
- *     responses:
- *       200:
- *         description: Successfully retrieved top colleges
- *       500:
- *         description: Internal server error
- */
 CollegeRouter.get("/top", getTopColleges as any);
-
-/**
- * @swagger
- * /api/v1/college/info/{id}:
- *   get:
- *     summary: Get college information by ID
- *     tags: [Colleges]
- *     description: Retrieve detailed information about a specific college
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: College ID
- *         schema:
- *           type: integer
- *           example: 1
- *     responses:
- *       200:
- *         description: Successfully retrieved college information
- *       404:
- *         description: College not found
- *       500:
- *         description: Internal server error
- */
 CollegeRouter.get("/info/:id", collegeInfo as any);
-
-/**
- * @swagger
- * /api/v1/college/courses/{id}:
- *   get:
- *     summary: Get courses offered by a college
- *     tags: [Colleges]
- *     description: Retrieve all courses offered by a specific college
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: College ID
- *         schema:
- *           type: integer
- *           example: 1
- *     responses:
- *       200:
- *         description: Successfully retrieved college courses
- *       404:
- *         description: College not found
- *       500:
- *         description: Internal server error
- */
 CollegeRouter.get("/courses/:id", getCollegeCoursesInfo as any);
-
-/**
- * @swagger
- * /api/v1/college/fees/{id}:
- *   get:
- *     summary: Get fess for corses offered by a college
- *     tags: [Colleges]
- *     description: Retrieve all fees for courses offered by a specific college
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: College ID
- *         schema:
- *           type: integer
- *           example: 1
- *     responses:
- *       200:
- *         description: Successfully retrieved college courses fees
- *       404:
- *         description: College not found
- *       500:
- *         description: Internal server error
- */
 CollegeRouter.get("/fees/:id", getCollegeFeesInfo as any);
+
+CollegeRouter.get("/departments/:id", getCollegeDepartmentsInfo as any);
+CollegeRouter.get("/careers/:id", getCollegeCareersInfo as any);
+CollegeRouter.get("/ranking/:id", getCollegeRankingsInfo as any);
+CollegeRouter.get("/scholarship/:id", getCollegeScholarshipInfo as any);
+CollegeRouter.get("/placement/:id", getCollegePlacementInfo as any);
+CollegeRouter.get("/faq/:id", getCollegefaq as any);
 
 export default CollegeRouter;
